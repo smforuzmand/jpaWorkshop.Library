c59a7dd94bf59107d4f76852e8746b756605384c
Mohsen Forouzmand <career.forouzmand@gmail.com>
2022-09-14 10:10:28 +0200

    part1

---
+++ src/main/java/entity/AppUser.java
@@ -0,0 +1,110 @@
+package entity;
+
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Objects;
+
+
+@Entity
+public class AppUser {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int appUserId;
+
+    @Column(length = 250 ,nullable = false)
+    private String userName;
+    @Column(length = 150, nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private LocalDate regDate;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "detailsId")
+    private Details userDetails;
+
+    public AppUser(int appUserId, String userName, String password, LocalDate regDate, Details userDetails) {
+        this.appUserId = appUserId;
+        this.userName = userName;
+        this.password = password;
+        this.regDate = regDate;
+        this.userDetails = userDetails;
+    }
+
+    public AppUser(String userName, String password, LocalDate regDate, Details userDetails) {
+        this.userName = userName;
+        this.password = password;
+        this.regDate = regDate;
+        this.userDetails = userDetails;
+    }
+
+    public AppUser() {
+    }
+
+    public int getAppUserId() {
+        return appUserId;
+    }
+
+    public void setAppUserId(int appUserId) {
+        this.appUserId = appUserId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(LocalDate regDate) {
+        this.regDate = regDate;
+    }
+
+    public Details getUserDetails() {
+        return userDetails;
+    }
+
+    public void setUserDetails(Details userDetails) {
+        this.userDetails = userDetails;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AppUser user = (AppUser) o;
+        return appUserId == user.appUserId && Objects.equals(userName, user.userName) && Objects.equals(password, user.password) && Objects.equals(regDate, user.regDate) && Objects.equals(userDetails, user.userDetails);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(appUserId, userName, password, regDate, userDetails);
+    }
+
+    @Override
+    public String toString() {
+        return "AppUser{" +
+                "appUserId=" + appUserId +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", regDate=" + regDate +
+                ", userDetails=" + userDetails +
+                '}';
+    }
+}
---
+++ src/main/java/se/lexicon/jpaworkshop/JpaWorkshopApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.jpaworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaWorkshopApplication.class, args);
+    }
+
+}
